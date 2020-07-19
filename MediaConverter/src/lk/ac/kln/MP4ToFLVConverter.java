a94d22db491e8bdffd03ab374f7a91a291238bc7
Binguni19Edirisuriya <45828306+Binguni19Edirisuriya@users.noreply.github.com>
2020-07-19 16:07:15 +0530

    Update Mp4ToFlv converter class  to perform its conversion

--- MediaConverter/src/lk/ac/kln/MP4ToFLVConverter.java
+++ MediaConverter/src/lk/ac/kln/MP4ToFLVConverter.java
@@ -1,5 +1,9 @@
 package lk.ac.kln;
 
+import it.sauronsoftware.jave.*;
+
+import java.io.File;
+
 public class MP4ToFLVConverter implements Converter{
 
     public static MP4ToFLVConverter mp4ToFLVConverter;
@@ -15,6 +19,41 @@ public class MP4ToFLVConverter implements Converter{
 
     @Override
     public void Convert(String source, String output) {
-        System.out.println("MP4 --> MKV ==> To be implemented");
+
+        File sourceFile=new File(source);
+        File outputFile=new File(output);
+        File[] inputFiles=sourceFile.listFiles();
+
+        AudioAttributes audio = new AudioAttributes();
+        audio.setCodec("libmp3lame");
+        audio.setBitRate(new Integer(64000));
+        audio.setChannels(new Integer(1));
+        audio.setSamplingRate(new Integer(22050));
+        VideoAttributes video = new VideoAttributes();
+        video.setCodec("flv");
+        video.setBitRate(new Integer(160000));
+        video.setFrameRate(new Integer(25));
+        EncodingAttributes attrs = new EncodingAttributes();
+        attrs.setFormat("flv");
+        attrs.setAudioAttributes(audio);
+        attrs.setVideoAttributes(video);
+
+        Encoder encoder = new Encoder();
+
+        File mp4ToMFLVConverted=new File(output+"/mp4Toflv-Converted");
+
+        for(File file:inputFiles){
+            System.out.println("Converting file :"+ source);
+            //System.out.println(new File(output+(file.toString().replace(sourceFile.toString(),"//")).replace(".mp4",".mp3"))+"\n");
+            try {
+                //encoder.encode(file, new File(output+(file.toString().replace(sourceFile.toString(),"//")).replace(".mp4",".mp3")), attrs);
+                encoder.encode(file, new File(mp4ToMFLVConverted+(file.toString().replace(sourceFile.toString(),"//")).replace(".mp4",".flv")), attrs);
+                System.out.println("Conversion Successfull! FLV file Saved! in" + output);
+                // delete source file after conversion
+                file.delete();
+            } catch (EncoderException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
