9d40997f12fca66e01624cf8f27b4fccc1651409
Binguni19Edirisuriya <biguniemail@gmail.com>
2020-07-30 12:26:27 +0530

    re committing previously comitted coverter classes with modified path patterns to be worked on a windows desktop

--- MediaConverter/src/lk/ac/kln/MP4ToFLVConverter.java
+++ MediaConverter/src/lk/ac/kln/MP4ToFLVConverter.java
@@ -20,5 +20,41 @@ public class MP4ToFLVConverter implements Converter{
     @Override
     public void Convert(String source, String output) {
 
+        File sourceFile=new File(source);
+        File outputFile=new File(output);
+        File[] inputFiles=sourceFile.listFiles();
+
+        AudioAttributes audio = new AudioAttributes();
+        audio.setCodec("libmp3lame");
+        audio.setBitRate(new Integer(64000));
+        audio.setChannels(new Integer(1));
+        audio.setSamplingRate(new Integer(22050));
+        VideoAttributes video = new VideoAttributes();
+        video.setCodec("flv");
+        video.setBitRate(new Integer(160000));
+        video.setFrameRate(new Integer(25));
+        EncodingAttributes attrs = new EncodingAttributes();
+        attrs.setFormat("flv");
+        attrs.setAudioAttributes(audio);
+        attrs.setVideoAttributes(video);
+
+        Encoder encoder = new Encoder();
+
+        File mp4ToFLVConverted=new File(output+"/mp4Toflv-Converted");
+
+        for(File file:inputFiles){
+//            System.out.println("Converting file :"+ source);
+            //System.out.println(new File(output+(file.toString().replace(sourceFile.toString(),"//")).replace(".mp4",".mp3"))+"\n");
+            try {
+                //encoder.encode(file, new File(output+(file.toString().replace(sourceFile.toString(),"//")).replace(".mp4",".mp3")), attrs);
+                encoder.encode(file, new File(mp4ToFLVConverted+(file.toString().replace(sourceFile.toString(),"//")).replace(".mp4",".flv")), attrs);
+//                System.out.println("Conversion Successfull! FLV file Saved! in" + output);
+                System.out.println(file.toString()+" : Converted Successfully ");
+                // delete source file after conversion
+                file.delete();
+            } catch (EncoderException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
